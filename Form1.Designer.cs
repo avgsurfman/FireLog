b3baa3540ecdcaa933c869df2668ddd72e23f2cf
franc <franc@ZENBOOK>
2025-03-11 22:48:30 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace Lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.StatusText = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.listBox5 = new System.Windows.Forms.ListBox();
+            this.listBox6 = new System.Windows.Forms.ListBox();
+            this.buttonOpenFile = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.fullLogBox = new System.Windows.Forms.ListBox();
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.FileLocationTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(23, 402);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(158, 254);
+            this.listBox1.TabIndex = 0;
+            // 
+            // StatusText
+            // 
+            this.StatusText.AutoSize = true;
+            this.StatusText.Location = new System.Drawing.Point(18, 678);
+            this.StatusText.Name = "StatusText";
+            this.StatusText.Size = new System.Drawing.Size(115, 25);
+            this.StatusText.TabIndex = 1;
+            this.StatusText.Text = "StatusText";
+            this.StatusText.Click += new System.EventHandler(this.StatusText_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 25;
+            this.listBox2.Location = new System.Drawing.Point(187, 402);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(158, 254);
+            this.listBox2.TabIndex = 2;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 25;
+            this.listBox3.Location = new System.Drawing.Point(351, 402);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(158, 254);
+            this.listBox3.TabIndex = 3;
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.ItemHeight = 25;
+            this.listBox4.Location = new System.Drawing.Point(515, 402);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(164, 254);
+            this.listBox4.TabIndex = 4;
+            // 
+            // listBox5
+            // 
+            this.listBox5.FormattingEnabled = true;
+            this.listBox5.ItemHeight = 25;
+            this.listBox5.Location = new System.Drawing.Point(685, 402);
+            this.listBox5.Name = "listBox5";
+            this.listBox5.Size = new System.Drawing.Size(152, 254);
+            this.listBox5.TabIndex = 5;
+            // 
+            // listBox6
+            // 
+            this.listBox6.FormattingEnabled = true;
+            this.listBox6.ItemHeight = 25;
+            this.listBox6.Location = new System.Drawing.Point(843, 402);
+            this.listBox6.Name = "listBox6";
+            this.listBox6.Size = new System.Drawing.Size(171, 254);
+            this.listBox6.TabIndex = 6;
+            // 
+            // buttonOpenFile
+            // 
+            this.buttonOpenFile.Location = new System.Drawing.Point(685, 30);
+            this.buttonOpenFile.Name = "buttonOpenFile";
+            this.buttonOpenFile.Size = new System.Drawing.Size(134, 61);
+            this.buttonOpenFile.TabIndex = 7;
+            this.buttonOpenFile.Text = "Open File";
+            this.buttonOpenFile.UseVisualStyleBackColor = true;
+            this.buttonOpenFile.Click += new System.EventHandler(this.buttonOpenFile_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // fullLogBox
+            // 
+            this.fullLogBox.FormattingEnabled = true;
+            this.fullLogBox.ItemHeight = 25;
+            this.fullLogBox.Location = new System.Drawing.Point(23, 114);
+            this.fullLogBox.Name = "fullLogBox";
+            this.fullLogBox.Size = new System.Drawing.Size(991, 229);
+            this.fullLogBox.TabIndex = 8;
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Location = new System.Drawing.Point(23, 30);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(134, 61);
+            this.LoadButton.TabIndex = 9;
+            this.LoadButton.Text = "Load";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            this.LoadButton.Click += new System.EventHandler(this.LoadButton_Click);
+            // 
+            // FileLocationTextBox
+            // 
+            this.FileLocationTextBox.Location = new System.Drawing.Point(187, 45);
+            this.FileLocationTextBox.Name = "FileLocationTextBox";
+            this.FileLocationTextBox.Size = new System.Drawing.Size(462, 31);
+            this.FileLocationTextBox.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 365);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(182, 365);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 25);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(346, 365);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 25);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(510, 365);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 25);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(680, 365);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 25);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(838, 365);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 25);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "label6";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1060, 728);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.FileLocationTextBox);
+            this.Controls.Add(this.LoadButton);
+            this.Controls.Add(this.fullLogBox);
+            this.Controls.Add(this.buttonOpenFile);
+            this.Controls.Add(this.listBox6);
+            this.Controls.Add(this.listBox5);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.StatusText);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label StatusText;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.ListBox listBox5;
+        private System.Windows.Forms.ListBox listBox6;
+        private System.Windows.Forms.Button buttonOpenFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox fullLogBox;
+        private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.TextBox FileLocationTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Lab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            List<string> Type = new List<string>();
+            List<string> Date = new List<string>();
+            List<string> Time = new List<string>();
+            List<string> Source = new List<string>();
+            List<string> Destination = new List<string>();
+            List<string> Transport = new List<string>();
+        }
+
+        private void StatusText_Click(object sender, EventArgs e)
+        {
+            this.StatusText.Text = "Boop!";
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void buttonOpenFile_Click(object sender, EventArgs e)
+        {
+            // Show the open file dialog
+            DialogResult result = openFileDialog1.ShowDialog();
+            // I need the fucking signal somehow
+            if (result == DialogResult.OK)
+            {
+                string file = openFileDialog1.FileName;
+                this.StatusText.Text = "File selected:" + file;
+                this.FileLocationTextBox.Text = file;   
+            }
+            
+        }
+
+        private void LoadButton_Click(object sender, EventArgs e)
+        {
+            const Int32 BufferSize = 512; // sector size on Windows
+            try { 
+                //update to using statement
+                FileStream fs = new FileStream(this.FileLocationTextBox.Text, FileMode.Open, FileAccess.Read);
+                StreamReader sr = new StreamReader(fs, System.Text.Encoding.UTF8); // append utf-8 in the future
+                
+
+                this.fullLogBox.Items.Clear();
+                String line;
+                while ((line = sr.ReadLine()) != null) { 
+                    this.fullLogBox.Items.Add(line);
+                    line.Split
+                }
+                sr.Close();
+
+            }
+            catch (IOException err)
+            {
+                this.StatusText.Text = String.Format("Error: {err}", err.Message);
+            }
+        }
+    }
+}
