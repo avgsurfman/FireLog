bb78e196eb08682a630c7ffe785aec5dfaa59322
Karol Madraszek <karol.madraszek.03@gmail.com>
2025-03-12 17:48:28 +0100

    Dodanie przycisku About i wyświetlenie liczby linii (ostateczna wersja pliku)

---
+++ AboutForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Lab2_WHDIBW
+{
+    public partial class AboutForm : Form
+    {
+        public AboutForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AboutForm_Load(object sender, EventArgs e)
+        {
+            labelInfo.Text = "Firewall logging util for the \"Introduction to Data Warehouses\"\n" +
+                             "course at the University of Zielona Góra\n\n" +
+                             "Authors:\n" +
+                             "- Karol Mądraszek\n" +
+                             "- Franciszek Moszczuk\n\n" +
+                             "2025";
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
